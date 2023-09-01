412c75deb1da65b8f28439d511dd5f3c88bf0f2c
Fahad Ahmed Chowdhury <fahadchy24@gmail.com>
2023-09-01 19:10:14 +0600

    Category added

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from 'axios';
+import { ref } from 'vue';
+
+const CategoryList = ref(null)
+
+getPostCategories()
+async function getPostCategories() {
+  const URL = 'https://basic-blog.teamrabbil.com/api/post-categories'
+  const res = await axios.get(URL)
+  if (res.status === 200) {
+    CategoryList.value = res.data
+  }
+
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">MR BLOG</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse me-4" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li v-for="item in CategoryList" class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">{{ item.name }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container">
+    <div class="row">
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
